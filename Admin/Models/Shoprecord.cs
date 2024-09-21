4b2cabce9ee8849b8296bb6cbd9b9f05778dcb68
erwrervv <erwrervv@gmail.com>
2024-09-21 20:53:30 +0800

    Model Admin

---
+++ Admin/Models/Shoprecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Travel.Admin.Models;
+
+public partial class Shoprecord
+{
+    public int ShopRecordid { get; set; }
+
+    public string? MemberName { get; set; }
+
+    public bool? ExchangeStatus { get; set; }
+
+    public DateTime? PurchaseTime { get; set; }
+
+    public int? TotalPrice { get; set; }
+
+    public string? MemberPhone { get; set; }
+
+    public string? Address { get; set; }
+
+    public int? MallProductTableId { get; set; }
+
+    public int? MallProductQuantity { get; set; }
+}
