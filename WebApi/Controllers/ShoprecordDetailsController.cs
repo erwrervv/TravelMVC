fab0d0fcf80f36492bafb1f645bcfcf8b5a20902
bb <bb090224jp@gmail.com>
2024-09-22 20:21:06 +0800

    0922_updateshopcontroller

---
+++ WebApi/Controllers/ShoprecordDetailsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Travel.WebApi.Models;
+using Travel.WebApi.DTO;
+
+namespace Travel.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoprecordDetailsController : ControllerBase
+    {
+        private readonly FinalContext _context;
+
+        public ShoprecordDetailsController(FinalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ShoprecordDetails
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ShoprecordDetail>>> GetShoprecordDetails()
+        {
+            return await _context.ShoprecordDetails.ToListAsync();
+        }
+
+        // GET: api/ShoprecordDetails/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ShoprecordDetail>> GetShoprecordDetail(int id)
+        {
+            var shoprecordDetail = await _context.ShoprecordDetails.FindAsync(id);
+
+            if (shoprecordDetail == null)
+            {
+                return NotFound();
+            }
+
+            return shoprecordDetail;
+        }
+
+        // PUT: api/ShoprecordDetails/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShoprecordDetail(int id, ShoprecordDetail shoprecordDetail)
+        {
+            if (id != shoprecordDetail.ShoprecordDetailid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(shoprecordDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShoprecordDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ShoprecordDetails
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ShoprecordDetailDTO>> PostShoprecord(ShoprecordDetailDTO dto)
+        {
+            var shoprecordDetail = new ShoprecordDetail
+            {
+               
+                MallProductName = dto.MallProductName,
+                MallProductQuantity = dto.MallProductQuantity,
+               MallProductTableId =dto.MallProductTableId,
+
+    };
+
+            // 将新的实体添加到数据库
+            _context.ShoprecordDetails.Add(shoprecordDetail);
+            await _context.SaveChangesAsync();
+
+            // 返回创建的资源，包含新生成的 ID
+            return CreatedAtAction("GetShoprecordDetail", new { id = shoprecordDetail.ShoprecordDetailid }, shoprecordDetail);
+        }
+
+        // DELETE: api/ShoprecordDetails/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShoprecordDetail(int id)
+        {
+            var shoprecordDetail = await _context.ShoprecordDetails.FindAsync(id);
+            if (shoprecordDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.ShoprecordDetails.Remove(shoprecordDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ShoprecordDetailExists(int id)
+        {
+            return _context.ShoprecordDetails.Any(e => e.ShoprecordDetailid == id);
+        }
+    }
+}
---
+++ WebApi/DTO/ShoprecordDetailDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Travel.WebApi.DTO
+{
+    public class ShoprecordDetailDTO
+    {
+
+
+        public int MallProductTableId { get; set; }
+        public string MallProductName { get; set; }
+
+        public int? MallProductQuantity { get; set; }
+
+       
+    }
+}
